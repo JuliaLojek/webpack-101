f217c2cde63f41026f63bfc8908567780f956cd1
Julia Lojek <julialojek@gmail.com>
2020-04-14 10:57:38 +0200

    html plugin added

--- webpack-react/webpack.config.js
+++ webpack-react/webpack.config.js
@@ -1,21 +1,27 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'script.js'
-    },
-    module: {
-      rules: [{
-          test: /\.jsx?$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/,
-          query: {
-              cacheDirectory: true,
-              presets: ["@babel/preset-env", "@babel/preset-react"]
-          }
-      }]
-  }
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'script.js'
+  },
+  module: {
+    rules: [{
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+            cacheDirectory: true,
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+        }
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })
+  ]
 }
